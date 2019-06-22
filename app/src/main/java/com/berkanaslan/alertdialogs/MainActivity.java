eced9bef0581863602b40e777e31e564e2c0ca14
Berkan ASLAN <info@berkanaslan.com>
2019-06-23 01:03:13 +0300

    A08-AlertDialogs

---
+++ app/src/main/java/com/berkanaslan/alertdialogs/MainActivity.java
@@ -0,0 +1,56 @@
+package com.berkanaslan.alertdialogs;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Contex
+        //Activity Contex
+        //App Contex
+
+        //getApplicationContext()
+
+
+
+        Toast.makeText(MainActivity.this, "Toast message", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void save(View view) {
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Save");
+        alert.setMessage("Are u sure?");
+        
+        //SAVE
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+               Toast.makeText(MainActivity.this, "Succesful.", Toast.LENGTH_LONG).show();
+            }
+        });
+        
+        //NOT SAVE
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Not saved", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alert.show();
+
+    }
+}
